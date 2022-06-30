99c2c01640b0e11ebfb25124986371177b923085
paulojamero <32075775+paulojamero@users.noreply.github.com>
2022-06-30 23:07:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+//event listener
+let numberInput = document.querySelector('#numberInput');
+// numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
+
+
+//AJAX / XHR to work with API
+// function getFactAjax(){
+//     let number = numberInput.value;
+    
+
+//     //REQUEST to API
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://numbersapi.com/'+number);
+    
+//     xhr.onload = function(){
+//         if(this.status == 200 && number != ''){
+//             fact.style.display = 'block';
+//             factText.innerText = this.responseText;
+//         }
+//     }
+//     xhr.send();
+// }
+
+function getFactFetch(){
+    let number = numberInput.value;
+
+    fetch('http://numbersapi.com/'+number)
+        .then(response => response.text())
+        .then(data => {
+            if(number != ''){
+                fact.style.display = 'block';
+                factText.innerText = data;
+            }
+        })
+        .catch(err => console.log(err));
+}
+    
