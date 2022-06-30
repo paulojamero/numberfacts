08379aab3d56bf1a1f0f508fbf777d16d12617af
paulojamero <32075775+paulojamero@users.noreply.github.com>
2022-06-30 23:20:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ numberInput.addEventListener('input', getFactFetch);
 function getFactFetch(){
     let number = numberInput.value;
 
-    fetch('https://numbersapi.com/'+number)
+    fetch('https://cors-anywhere.herokuapp.com/http://numbersapi.com/'+number)
         .then(response => response.text())
         .then(data => {
             if(number != ''){
